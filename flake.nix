0d07094d792a1990a6b669b5d43a70e6855dd357
Michael Smith <m@michaelsmith.be>
2025-01-02 11:51:30 +0100

    Add helper function to support multiple architectures (for Michael's aarch64-darwin Macbook)

--- flake.nix
+++ flake.nix
@@ -32,64 +32,89 @@
     tab-ui,
     ...
   } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [
-        devshell.overlays.default
-        inputs.tab-ui.overlays.default
-      ];
-    };
+    # This list of architectures provides the supported systems to the wrapper function below.
+    # It basically defines which architectures can manage the VoidWarranties infrastructure using this flake.
+    supportedSystems = [
+      "aarch64-darwin"
+      "x86_64-linux"
+    ];
+
+    # This helper function is used to make the flake outputs below more DRY. It looks a bit intimidating but that's
+    # mostly because of the functional programming nature of Nix. I recommend reading
+    # [Nix language basics](https://nix.dev/tutorials/nix-language.html) and search online for resources about
+    # functional programming paradigms.
+    #
+    # Basically this function makes it so that instead of declaring outputs for every architecture as the flake schema
+    # expects, e.g.:
+    #
+    # packages = {
+    #   "x86_64-linux" = {
+    #     ...
+    #   };
+    #   "aarch64-darwin" = {
+    #     ...
+    #   };
+    # };
+    #
+    # we can define each output below (package, formatter, ...) once for all the architectures / systems.
+    #
+    # See https://ayats.org/blog/no-flake-utils to learn more.
+    #
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        function (import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+            inputs.tab-ui.overlays.default
+          ];
+        }));
   in {
-    formatter."x86_64-linux" = pkgs.alejandra;
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-    packages = {
-      "x86_64-linux" = {
-        barputer-demo =
-          (inputs.nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              ({pkgs, ...}: {
-                nixpkgs.overlays = [
-                  inputs.backtab.overlays.default
-                ];
-              })
-              inputs.backtab.nixosModules.backtab
-              (import ./machines/barputer-demo {inherit pkgs;})
-            ];
-          })
-          .config
-          .system
-          .build
-          .vm;
-      };
-    };
+    packages = forAllSystems (pkgs: {
+      barputer-demo =
+        (inputs.nixpkgs.lib.nixosSystem {
+          system = pkgs.system;
+          modules = [
+            ({pkgs, ...}: {
+              nixpkgs.overlays = [
+                inputs.backtab.overlays.default
+              ];
+            })
+            inputs.backtab.nixosModules.backtab
+            (import ./machines/barputer-demo {inherit pkgs;})
+          ];
+        })
+        .config
+        .system
+        .build
+        .vm;
+    });
 
-    devShells = {
-      "x86_64-linux" = {
-        default = pkgs.devshell.mkShell {
-          name = "VoidWarranties infrastructure shell";
-          packages = with pkgs; [
-            nixos-rebuild
-          ];
-          commands = [
-            {
-              name = "barputer-deploy";
-              help = "deploys the barputer to a given host";
-              command = ''
-                host=$1
-                shift
-                nixos-rebuild --flake ".#$host" \
-                  --build-host $host \
-                  --target-host $host \
-                  --use-remote-sudo \
-                  --fast \
-                "$@"
-              '';
-            }
-          ];
-        };
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.devshell.mkShell {
+        name = "VoidWarranties infrastructure shell";
+        packages = with pkgs; [
+          nixos-rebuild
+        ];
+        commands = [
+          {
+            name = "barputer-deploy";
+            help = "deploys the barputer to a given host";
+            command = ''
+              host=$1
+              shift
+              nixos-rebuild --flake ".#$host" \
+                --build-host $host \
+                --target-host $host \
+                --use-remote-sudo \
+                --fast \
+              "$@"
+            '';
+          }
+        ];
       };
-    };
+    });
   };
 }
