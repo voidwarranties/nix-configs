41660ecf17acfa5ed60beca94b1c84b3437858c5
Koen De Voegt <koen@devoegt.be>
2025-02-05 21:28:47 +0100

    key koen

--- machines/barputer/default.nix
+++ machines/barputer/default.nix
@@ -49,6 +49,7 @@ in {
     authorizedKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILes7WTtBxDp1ILq+9iF1v2mmiQ0yFPprMREPUO240me"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJM++IIAAG4SJLCDRv3MQ/CoX9CQg/8XhQeJc2uevkv2 eline@stargazer"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAXyGbpul93Y7ibDPqEdZxw7Tt8gIaTOmTOdlSNOak8i koen@devoegt.be"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBa9WsiF3b7e6pszbbDtu7PuwiZemDcfI02n832tVcIX e12l@e12l.be"
     ];
   };
@@ -138,6 +139,7 @@ in {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILes7WTtBxDp1ILq+9iF1v2mmiQ0yFPprMREPUO240me m@Tatooine.local"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBa9WsiF3b7e6pszbbDtu7PuwiZemDcfI02n832tVcIX"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAXyGbpul93Y7ibDPqEdZxw7Tt8gIaTOmTOdlSNOak8i koen@devoegt.be"
       ];
     };
   };
